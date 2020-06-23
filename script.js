8f1853a20465851fe5eef8bee1729e55587d751e
Artem <example@email.com>
2020-06-23 12:46:26 +0200

    Transformed everything into functions and worked with the genre

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 'use strict';
 
-const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', "");
+    }
+}
+start();
 
 const obj = {
     count: numberOfFilms,
@@ -11,29 +20,49 @@ const obj = {
 };
 
 
-for (let i = 1; i < 3; i++) {
-    const lastMovie = prompt('Один из последних просмотренных фильмов?', ""),
-          rating = +prompt('Дайте ему оценку?', ""); 
+function rememberMyFilms() {
+    for (let i = 1; i < 3; i++) {
+        const lastMovie = prompt('Один из последних просмотренных фильмов?', ""),
+              rating = +prompt('Дайте ему оценку?', ""); 
 
-    if ( (lastMovie == false || rating == false) || (lastMovie == null || rating == null) || (lastMovie.length > 50 || rating.length > 50) ) {
-        i--;
-    } else {
-        obj.movies[lastMovie] = rating;
+        if (lastMovie == '' || rating == '' || lastMovie == null || rating == null || lastMovie.length > 50 || rating.length > 50) {
+            i--;
+        } else {
+            obj.movies[lastMovie] = rating;
+        }
     }
-    
 }
+rememberMyFilms();
 
-if (obj.count <= 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (obj.count > 10 && obj.count <= 30) {
-    console.log('Вы классический зритель');
-} else if (obj.count > 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+function detectPersonalLevel() {
+    if (obj.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (obj.count >= 10 && obj.count <= 30) {
+        console.log('Вы классический зритель');
+    } else if (obj.count > 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
 }
+detectPersonalLevel();
 
-console.log(obj);
+function showMyDB() {
+    if (obj.privat == false) {
+        console.log(obj);
+    }
+}
+showMyDB();
+
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        obj.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`);
+    }
+}
+writeYourGenres();
+
+// console.log(obj);
 
 
 
